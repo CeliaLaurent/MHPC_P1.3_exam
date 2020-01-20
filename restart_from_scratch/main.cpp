211f0ec92d0d381b4b39944aac9d0c241ded0295
CeliaLaurent <claurent@inogs.it>
2020-01-20 14:28:35 +0100

    add updates to temporary version

--- restart_from_scratch/main.cpp
+++ restart_from_scratch/main.cpp
@@ -1,60 +1,38 @@
 #include <iostream>
-#include <memory>  //unique_ptr
+#include <memory>  // For unique_ptr
 #include <utility>
-#include "bst.hpp"
 #include <vector>
 #include <map>
+#include <ctime>    // For time()
+#include <cstdlib>  // For srand() and rand()
+#include "bst.hpp"
 
 int main() {
+  /********** Construction of a bst object of type <int,int> **************************/
+  bst<int,int> bst_ii; 
 
-  //bst<int,std::string> b0{};
-  //auto it0=b0.insert({8,"ENDIAN"});
-//auto it = it0.first;
-  //*it.second = "ciao";
-//std::cout << "oleeeeeeeee " << it0.first->first << " " << it0.first->second << std::endl;
-//std::vector<typename bst<int,std::string>::nodetype> v(11);
-  
-  //std::vector<std::decay(decltype(*(it0.first)))> v(13);
-  bst<int,int> b1;
-  auto ita{b1.insert(std::make_pair(15,0))};
-  ita=b1.insert({8,0});
-  ita=b1.insert({12,0});
-  ita=b1.insert({11,0});
-  ita=b1.insert({5,0});
-  ita=b1.insert({25,0});
-  ita=b1.insert({2,0});
-  ita=b1.insert({17,0});
-  ita=b1.insert({24,0});
-  ita=b1.insert({6,0});
-  ita=b1.insert({12,0});
-  ita=b1.insert({27,0});
-  ita=b1.insert({19,0});
-  ita=b1.insert({14,0});
-  ita=b1.insert({3,0});
-  ita=b1.insert({7,0});
-  std::cout<<b1<<std::endl;
-//bst<int,std::string> b1{};
-//auto ita{b1.insert({5,"CHIEF"})};
-//auto itb=b1.insert({3,"l"});
-//itb=b1.emplace(10,"r");
-//itb=b1.insert({6,"rl"});
-//itb=b1.insert({4,"lr"});
-//itb=b1.insert({1,"ll"});
-//itb=b1.insert({7,"rlr"});
-//itb=b1.insert({8,"rlrr"});
-//itb=b1.insert({9,"rlrrr"});
-//itb=b1.insert({14,"rr"});
-//itb=b1.insert({13,"rrl"});
-//std::cout << *(b1.find(9)) << std::endl;
-  b1.balance();
-  std::cout<<b1<<std::endl;
-//std::vector<std::pair<const int, std::string>> v(12);
-//std::copy(b1.cbegin(), b1.cend(), v.begin());
-//for (auto x : v)
-//    std::cout << 1 << std::endl;
+  /********** Testing the insert member function **************************************/
+  srand(time(0));  // Initialize random number generator
+  auto key{rand()%30};	  
+  auto pair_r{bst_ii.insert({key,0})};    // insert the root pair of key, value=0
+  std::cout<<key<<" inserted as root"<<std::endl;
+  for(size_t value{1};value<50;value++){  // Try to insert up to 50 more pairs of key, value
+     key=rand()%30;	  
+     pair_r=bst_ii.insert({key,value});
+     if(pair_r.second)std::cout<<key<<" inserted successfully"<<std::endl;
+     else std::cout<<key<<" already exists in bst"<<std::endl;
+  }
 
- 
+  /********* Testing the operator <<,          ****************************************
+   ********* the balance member function       ****************************************
+   ********* and doing so, as well operator ++ *****************************************/
+  std::cout<<bst_ii<<std::endl;
+
+  bst_ii.balance();
+  std::cout<<bst_ii<<std::endl;
+
+
+
   std::cout << "MAIN ENDED NORMALLY"<< std::endl;
-
   return 0;
 }
